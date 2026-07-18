8522d913aff9a71cb717da28dde4bf61647fd634
scumpi <saberfang961@gmail.com>
2026-07-18 13:49:07 +0200

    feat: implement G2

--- script_resources/cards/effects/add_extra_activations_effect.gd
+++ script_resources/cards/effects/add_extra_activations_effect.gd
@@ -8,7 +8,17 @@ extends CardEffect
 
 
 func resolve(
-	_context: BattleContext,
-	_slot: ChainSlotState,
+	context: BattleContext,
+	slot: ChainSlotState,
 ) -> void:
-	pass
+	var next_index: int = slot.slot_index + 1
+
+	if next_index >= context.chain_slot_states.size():
+		return
+
+	var next_slot: ChainSlotState = context.chain_slot_states[next_index]
+
+	if not next_slot.is_active():
+		return
+
+	next_slot.activation_count += extra_count
