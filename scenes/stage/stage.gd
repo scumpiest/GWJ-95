ea7bf382b2aac239e3aef330d1f7f78f503ca7e0
Zaxiure <daniel@voorn.dev>
2026-07-19 00:03:45 +0200

    feat: add boss stage

--- scenes/stage/stage.gd
+++ scenes/stage/stage.gd
@@ -5,4 +5,10 @@ extends Node2D
 
 
 func _ready() -> void:
+	LevelManager.next_level.connect(_check_boss_level)
 	_sprite.get_animation_state().set_animation("normal_level", true, 0)
+
+func _check_boss_level() -> void:
+	if LevelManager.current_level.type == Level.LevelType.BOSS:
+		_sprite.get_animation_state().set_animation("boss_cutscene", false, 0)
+		_sprite.get_animation_state().add_animation("bossl_level", 4, true, 0)
--- script_resources/level.gd
+++ script_resources/level.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name Level
 
-enum LevelType { GAME, SHOP }
+enum LevelType { GAME, BOSS, SHOP }
 
 @export var type: LevelType = LevelType.GAME
 @export var enemy_scene: PackedScene
