b775db20cac3726680e4d51a164d5dd4d4f9b013
scumpi <saberfang961@gmail.com>
2026-07-18 16:18:46 +0200

    feat: finally done with all the cards

--- script_resources/cards/effects/chain_modifier_effect.gd
+++ script_resources/cards/effects/chain_modifier_effect.gd
@@ -14,7 +14,19 @@ extends CardEffect
 
 
 func resolve(
-	_context: BattleContext,
-	_slot: ChainSlotState,
+	context: BattleContext,
+	slot: ChainSlotState,
 ) -> void:
-	pass
+	if context.coun_adjacent_same_color(slot.slot_index, require_adjacent_color) <= 0:
+		slot.skip_activation = true
+		return
+
+	for i in range(slot.slot_index + 1, context.chain_slot_states.size()):
+		var other_slot: ChainSlotState = context.chain_slot_states[i]
+		if not other_slot.is_active():
+			continue
+
+		if other_slot.color == skip_color_after:
+			other_slot.skip_activation = true
+		elif other_slot.color == double_color_after:
+			other_slot.activation_count = 2
